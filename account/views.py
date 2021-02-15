e7c802e8653a90aec726603d940cdacdc601537d
jrkd5 <jaroslaw.dulinski@gmail.com>
2021-02-15 19:33:36 +0100

    Added create_action to views.

--- account/views.py
+++ account/views.py
@@ -8,6 +8,7 @@ from django.views.decorators.http import require_POST
 
 from account.forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from account.models import Profile, Contact
+from actions.utils import create_action
 from common.decorators import ajax_required
 
 
@@ -46,6 +47,7 @@ def register(request):
             new_user.save()
             # Create the user profile
             Profile.objects.create(user=new_user)
+            create_action(new_user, 'has created an account')
             return render(request, 'account/register_done.html', {'new_user': new_user})
     else:
         user_form = UserRegistrationForm()
@@ -99,6 +101,7 @@ def user_follow(request):
                 Contact.objects.get_or_create(
                     user_from=request.user,
                     user_to=user)
+                create_action(request.user, 'is following', user)
             else:
                 Contact.objects.filter(user_from=request.user, user_to=user).delete()
             return JsonResponse({'status': 'ok'})
