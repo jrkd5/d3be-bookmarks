cfe25b29602dfeeb9113bddb31b8277a33a72a81
jrkd5 <jaroslaw.dulinski@gmail.com>
2021-02-15 16:56:38 +0100

    Created 'Actions' app.

--- d3be_bookmarks/settings.py
+++ d3be_bookmarks/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'account.apps.AccountConfig',
     'images.apps.ImagesConfig',
+    'actions.apps.ActionsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
